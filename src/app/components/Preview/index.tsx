03b564e89b2f2e911298a889d4d641c8816e0232
Peter Pistorius <peter.pistorius@gmail.com>
2025-08-23 13:51:12 +0200

    Filter out port 8910 for now.

--- src/app/components/Preview/index.tsx
+++ src/app/components/Preview/index.tsx
@@ -2,11 +2,18 @@ import { env } from "cloudflare:workers";
 import { getSandbox } from "@cloudflare/sandbox";
 
 export async function Preview({ containerId }: { containerId: string }) {
-  // const sandbox = getSandbox(env.Sandbox, containerId);
-  // const ports = await sandbox.getExposedPorts("localhost");
-  // TODO: Adding the port here should not be required.
-  // const url = ports[0].url.substring(0, ports[0].url.length - 1) + ":5173/";
-  return <div>test</div>;
+  const sandbox = getSandbox(env.Sandbox, containerId);
+  let ports = await sandbox.getExposedPorts("localhost");
 
-  // return <iframe src={url} className="flex-1" />;
+  ports = ports.filter((p) => p.port !== 8910);
+
+  // TODO: Adding the port here should not be required.
+  const url = ports[0].url.substring(0, ports[0].url.length - 1) + ":5173/";
+  return (
+    <iframe
+      src={url}
+      className="flex-1 h-full"
+      sandbox="allow-scripts allow-same-origin"
+    />
+  );
 }
